8ee1c81b982fa3e54d92a9524aaabbd41ac08a10
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-06-08 23:20:08 +0530

    .

--- src/difarraylinked.java
+++ src/difarraylinked.java
@@ -6,25 +6,39 @@ public class difarraylinked {
         LinkedList<Integer> linkedList = new LinkedList<Integer>();
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
-            long startTime;
-            long endTime;
-            long elepsedTime;
+        long startTime;
+        long endTime;
+        long elepsedTime;
 
-            for (int i = 0; i < 1000000; i++) {
-                linkedList.add(i);
-                arrayList.add(i);
-            }
+        for (int i = 0; i < 1000000; i++) {
+            linkedList.add(i);
+            arrayList.add(i);
+        }
 
-            //linked list ******
-            startTime = System.nanoTime();
+        // linked list ******
+        startTime = System.nanoTime();
 
-            //do something
+        // do something
 
-            linkedList.get(0);
+        // linkedList.add(0);
+        // linkedList.get(999999);
 
-            endTime = System.nanoTime();
-            elepsedTime = endTime - startTime;
+        linkedList.remove(0);
 
-            System.out.println("linkedlist:\t"+ elepsedTime + "ns");
+        endTime = System.nanoTime();
+        elepsedTime = endTime - startTime;
+
+        System.out.println("linkedlist:\t" + elepsedTime + "ns");
+
+        // arraylist*******
+        startTime = System.nanoTime();
+        // arrayList.add(0);
+        arrayList.get(999999);
+
+        endTime = System.nanoTime();
+        elepsedTime = endTime - startTime;
+
+        System.out.println("arrayList:\t" + elepsedTime + "ns");
+
     }
 }
